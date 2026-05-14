e27e325b06e12d09d8fc45bb50a5ac3e14d9cb5b
Eric <kamaueric61@gmail.com>
2026-05-15 01:31:37 +0300

    asiggn 1

---
+++ intro.js
@@ -0,0 +1,9 @@
+const name = 'Eric Kamau';
+const age = 23;
+const favproglang = 'Javascript';
+
+console.log("Name:", name);
+console.log("Age:", age);
+console.log("Favorite Programming Language:", favproglang);
+
+
