5e2bc3d31d22059e55e3b0bf499477189af12701
Eric Kamau <kamaueric61@gmail.com>
2026-05-15 02:17:15 +0300

    Delete intro.js

--- intro.js
+++
@@ -1,9 +0,0 @@
-const name = 'Eric Kamau';
-const age = 23;
-const favproglang = 'Javascript';
-
-console.log("Name:", name);
-console.log("Age:", age);
-console.log("Favorite Programming Language:", favproglang);
-
-
